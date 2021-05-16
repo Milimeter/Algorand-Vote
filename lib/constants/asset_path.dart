b6cd0f7892af9f53fa7cccfa128add6cc0d3e83f
Milimeter <razaqolad300@gmail.com>
2021-05-16 18:01:48 -0500

    to main

---
+++ lib/constants/asset_path.dart
@@ -0,0 +1,11 @@
+final String imageAssetRoot = "assets/images/";
+final String a = _getImagePath("a.png");
+final String b = _getImagePath("b.png");
+final String c = _getImagePath("c.png");
+final String sisu = _getImagePath("sisu.png");
+final String login = _getImagePath("login.png");
+final String signup = _getImagePath("signup.png");
+final String cast = _getImagePath("cast.png");
+final String conduct = _getImagePath("conduct.png");
+
+String _getImagePath(String imageName) => imageAssetRoot + imageName;
